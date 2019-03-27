5035f9f116e101f836775c901629743d5a87a803
lmarquette <l_warugi@hotmail.com>
2019-03-26 20:22:45 -0500

    1

--- TTS/main.cpp
+++ TTS/main.cpp
@@ -2,7 +2,8 @@
 #include <sphelper.h>
 #include <atlbase.h>
 #include <iostream>
-#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string>
 #pragma warning(disable : 4996)
 using namespace std;
@@ -10,55 +11,30 @@ using namespace std;
 //https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ee431810(v=vs.85)
 //https://stackoverflow.com/questions/24846635/using-spvoice-in-c-windows
 
-
-void lines(/*char **lines, int *n_lines,*/ const char *filename)
+//How to read lines from a file and store them into an array using C
+//use fgets and strcpy to lines
+int ReadingLines(char **lines, const char *filename)
 {
-	string line;
-	ifstream readme;
-	readme.open(filename);
-	while ( getline(readme, line))
-	{
-		cout << line << endl;
-	}
-	readme.close();
+	FILE *fp = fopen(filename, "r");
+	int line_count = 0;
 
-	cout << "File Closed"<<endl;
+	char buffer[10000];
+	lines = (char**)malloc(sizeof(char*) * 1024);
+
+	while (fgets(buffer, sizeof(buffer), fp) != NULL)//Run until it reaches the end of the text file
+	{
+			printf("%s", buffer);
+			strcpy(*lines, buffer); //storing from buffer to lines pointer
+
+	}
+
+	return 0;
 }
 
 
 int main(int argc, char* argv[])
 {
-	//lines("test.txt");
-
-	string line;
-	ifstream readme;
-	readme.open("test.txt");
-	while (getline(readme, line))
-	{
-		cout << line << endl;
-	}
-	readme.close();
-
-
-	/*
-	HRESULT hr = S_OK;
-	CComPtr<ISpObjectTokenCategory> cpSpCategory = NULL;
-	if (SUCCEEDED(hr = SpGetCategoryFromId(SPCAT_VOICES, &cpSpCategory)))
-	{
-		CComPtr<IEnumSpObjectTokens> cpSpEnumTokens;
-		if (SUCCEEDED(hr = cpSpCategory->EnumTokens(L"Name=Microsoft Anna", NULL, &cpSpEnumTokens)))
-		{
-			CComPtr<ISpObjectToken> pSpTok;
-			while (SUCCEEDED(hr = cpSpEnumTokens->Next(1, &pSpTok, NULL)))
-			{
-				// do something with the token here; for example, set the voice
-				//pVoice->SetVoice(pSpTok, FALSE);
-				// NOTE:  IEnumSpObjectTokens::Next will *overwrite* the pointer; must manually release
-				pSpTok.Release();
-			}
-
-		}*/
-
+	char **line = new char *[10000];
 
 		//init, do this once
 		ISpVoice * pVoice = NULL;
@@ -69,11 +45,12 @@ int main(int argc, char* argv[])
 		//keep saying things here in a loop
 		const wchar_t *input = L"Gorillas live in Central Africa. There are two main species of gorilla, the Eastern Gorilla and the Western Gorilla. The Western Gorilla lives in Western Africa in countries such as Cameroon, the Congo, the Central African Republic, and Gabon. The Eastern Gorilla lives in Eastern African countries such as Uganda and Rwanda.";
 		//pVoice->Speak(input, 0, NULL);
-
-		wstring stemp = wstring(line.begin(), line.end()); //stores string into SAPI accessible variable 
-		LPCWSTR sw = (LPCWSTR)stemp.c_str(); //variable allowing SAPI to speak stored string
-		pVoice->Speak(sw, SPF_DEFAULT, NULL);
-
+		ReadingLines(line, "test.txt");
+		
+		//wstring stemp = wstring(line.begin(), line.end()); //stores string into SAPI accessible variable 
+		//LPCWSTR sw = (LPCWSTR)stemp.c_str(); //variable allowing SAPI to speak stored string
+		//pVoice->Speak(sw, SPF_DEFAULT, NULL);
+		
 		//pitch change
 		//pVoice->Speak(L"This sounds normal <pitch middle = '-600'/> but the pitch drops half way through", SPF_IS_XML, NULL);
 		//end of loop
@@ -83,9 +60,8 @@ int main(int argc, char* argv[])
 		pVoice->Release();
 		pVoice = NULL;
 		CoUninitialize();
-		
 	
 
-	system("pause");
-	return 0;
+		getchar();
+		return 0;
 }
