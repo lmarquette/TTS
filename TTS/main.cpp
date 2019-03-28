20a8ca056c8e36d39f6c643dc9e377354f57d662
Leo <l_warugi@hotmail.com>
2019-03-28 03:33:03 -0500

    1

--- TTS/main.cpp
+++ TTS/main.cpp
@@ -17,14 +17,15 @@ int ReadingLines(char **lines, const char *filename)
 {
 	FILE *fp = fopen(filename, "r");
 	int line_count = 0;
-
-	char buffer[10000];
-	lines = (char**)malloc(sizeof(char*) * 1024);
+	const long long int buffer_size = 10000;
+	char buffer[buffer_size];
+	lines = (char**)malloc(sizeof(char*) * buffer_size);
 
 	while (fgets(buffer, sizeof(buffer), fp) != NULL)//Run until it reaches the end of the text file
 	{
-			printf("%s", buffer);
-			strcpy(lines[line_count], buffer); //storing from buffer to lines pointer
+			lines[line_count] = buffer;
+			printf("%s", lines[line_count]);
+			//strcpy(lines[line_count], buffer); //storing from buffer to lines pointer
 			line_count++;
 	}
 
